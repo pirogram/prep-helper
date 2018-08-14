c5ecccbd8d95b9fb0591e87940e1c6c3d86f33b7
Manas Garg <manasgarg@gmail.com>
2018-08-14 14:26:42 -0700

    initial commit

---
+++ spec/prep-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PrepHelperView from '../lib/prep-helper-view';
+
+describe('PrepHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
